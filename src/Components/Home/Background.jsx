75d282b01580d8118504ea91d87520f11874a93d
Muhammad Junaid <junaid.aurangzeb1@gmail.com>
2025-08-28 20:12:11 +0500

    updated

--- src/Components/Home/Background.jsx
+++ src/Components/Home/Background.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Background = () => {
   return (
     <div className='w-full h-full'>
-        <video className='w-full h-full object-cover' autoPlay loop muted src="../../public/bg-video/83c745cf.mp4"></video>
+      <video className='w-full h-full object-cover' autoPlay loop muted src="/bg-video/83c745cf.mp4"></video>
     </div>
   )
 }
